6695436124e5fb2657f6229846c67bcdb4250558
Biswajit Nag <biswajitnag68@gmail.com>
2022-01-06 23:10:35 +0530

    music play

---
+++ script.js
@@ -0,0 +1,89 @@
+function effect1() {
+    const sound=document.getElementById("d1");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect2() {
+    const sound=document.getElementById("d2");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect3() {
+    const sound=document.getElementById("d3");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect4() {
+    const sound=document.getElementById("d4");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect5() {
+    const sound=document.getElementById("d5");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect6() {
+    const sound=document.getElementById("d6");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect7() {
+    const sound=document.getElementById("d7");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect8() {
+    const sound=document.getElementById("d8");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect9() {
+    const sound=document.getElementById("d9");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect10() {
+    const sound=document.getElementById("d10");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect11() {
+    const sound=document.getElementById("d11");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect12() {
+    const sound=document.getElementById("d12");
+    sound.currentTime=0;
+    sound.play();
+}function effect13() {
+    const sound=document.getElementById("d13");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect14() {
+    const sound=document.getElementById("d14");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect15() {
+    const sound=document.getElementById("d15");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect16() {
+    const sound=document.getElementById("d16");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect17() {
+    const sound=document.getElementById("d17");
+    sound.currentTime=0;
+    sound.play();
+}
+function effect6() {
+    const sound=document.getElementById("d6");
+    sound.currentTime=0;
+    sound.play();
+}
